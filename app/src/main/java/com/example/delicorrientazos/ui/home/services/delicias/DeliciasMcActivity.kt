8b39cc9cac7330e0a0998b633a3acc378df92d06
Zetian612 <jsperdomo@misena.edu.co>
2022-09-01 16:59:50 -0500

    Agregar modulo de delicias mc

---
+++ app/src/main/java/com/example/delicorrientazos/ui/home/services/delicias/DeliciasMcActivity.kt
@@ -0,0 +1,12 @@
+package com.example.delicorrientazos.ui.home.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.delicorrientazos.R
+
+class DeliciasMcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delicias_mc)
+    }
+}
