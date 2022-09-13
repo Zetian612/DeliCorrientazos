f80734179a2edd018e921a5993c697b77a37e59a
Zetian612 <jsperdomo@misena.edu.co>
2022-09-13 16:25:03 -0500

    Agregar logica de registro y inicio de sesion

--- app/src/main/java/com/example/delicorrientazos/MainActivity.kt
+++ app/src/main/java/com/example/delicorrientazos/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.delicorrientazos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.delicorrientazos.databinding.ActivityMainBinding
@@ -20,10 +21,19 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-
         navView.setupWithNavController(navController)
 
+    }
 
+    // mostrar mensaje de bienvenida despues de iniciar sesion
+    override fun onStart() {
+        super.onStart()
+        val bundle = intent.extras
+        if (bundle != null) {
+            val email = bundle.getString("email")
+            val provider = bundle.getString("provider")
 
+            Toast.makeText(this, "Bienvenido $email", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/delicorrientazos/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/delicorrientazos/ui/auth/RegisterActivity.kt
@@ -1,12 +1,60 @@
 package com.example.delicorrientazos.ui.auth
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.delicorrientazos.MainActivity
 import com.example.delicorrientazos.R
+import com.example.delicorrientazos.ui.account.AccountFragment
+import com.example.delicorrientazos.ui.account.ProviderType
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        signup()
+    }
+
+    private fun signup() {
+        title = "Autenticación"
+        val signupButton = findViewById<Button>(R.id.signupButton)
+        val nameUser = findViewById<EditText>(R.id.nameUser)
+        val lastnameUser = findViewById<EditText>(R.id.lastnameUser)
+        val signupEmailAddress = findViewById<EditText>(R.id.signupEmailAddress)
+        val signupPassword = findViewById<EditText>(R.id.signupPassword)
+
+        signupButton.setOnClickListener {
+            if (nameUser.text.isNotEmpty() && lastnameUser.text.isNotEmpty() && signupEmailAddress.text.isNotEmpty() && signupPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(signupEmailAddress.text.toString(), signupPassword.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error registrando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
